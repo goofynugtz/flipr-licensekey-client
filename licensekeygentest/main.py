834eceb4f08de39f6f130e341052a002d9ca0cac
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-09 16:55:40 +0530

    Added User email id

--- licensekeygentest/main.py
+++ licensekeygentest/main.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 class library():
-  def __init__(self, keygen_account_id, license_key):
+  def __init__(self, keygen_account_id, license_key, email):
     machine_fingerprint = machineid.hashed_id('app-name')
     validation = requests.post(
       f"https://api.keygen.sh/v1/accounts/{keygen_account_id}/licenses/actions/validate-key",
@@ -13,7 +13,7 @@ class library():
       },
       data=json.dumps({
         "meta": {
-          "scope": { "fingerprint": machine_fingerprint },
+          "scope": { "user": f"{email}" },
           "key": license_key
         }
       })
@@ -30,10 +30,20 @@ class library():
     validation_code = validation["meta"]["code"]
     print(">> Validation Code:", validation_code)
     invalid_code =  validation_code == "SUSPENDED" or \
-                    validation_code == "NOT_FOUND"
+                    validation_code == "NOT_FOUND" or \
+                    validation_code == "OVERDUE"   or \
+                    validation_code == "EXPIRED"   or \
+                    validation_code == "USER_SCOPE_MISMATCH"
+                    
     
     if invalid_code:
       if validation_code == "SUSPENDED":
         raise Exception("License is suspended. Please contact the library administrator.")
       if validation_code == "NOT_FOUND":
         raise Exception("Invalid License Key. Please recheck license_key")
+      if validation_code == "OVERDUE":
+        raise Exception("Invalid License Key. Key is Overdue.")
+      if validation_code == "EXPIRED":
+        raise Exception("License Key has been expired.")     
+      if validation_code == "USER_SCOPE_MISMATCH":
+        raise Exception("Incorrect email id. Verify email id.")
