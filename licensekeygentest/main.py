257c09c78362045eb4b549ebe94ffa5ccfd7e06d
goofynugtz <rahulranjan25.rr@gmail.com>
2023-05-25 15:39:09 +0530

    Obfyscation++

--- licensekeygentest/main.py
+++ licensekeygentest/main.py
@@ -1,8 +1,10 @@
 from threading import Thread as _______
 from _thread import interrupt_main as ___________
 from requests import get as ___, ConnectionError as _____
+from codecs import decode as ________________
 from json import dumps as ____
 from time import sleep as _________
+from base64 import b85decode as __________
 
 class library():
 
@@ -14,14 +16,14 @@ class library():
     while (True):
       try:
         ____________________________________________________________________________________________________________________________________________________________________________________ = ___(
-          f"https://licensing.sr.flipr.ai/api/actions/validate/",
+          __________(b'XmoUNb2=|CY-wX<ZgXjFXD)McE@o_LaB?nTX)j@LX)j@8bZKvHb1!ybY-wa+bY(9'),
           headers={
-            "Content-Type": "application/json",
-            "Accept": "application/json"
+            ________________(__________(b'LvL<$Wo~pWRC#b^')): ________________(__________(b'VQ_G4X=7n@X>V>XYIARH')),
+            ________________(__________(b'L1SZOaC8')): ________________(__________(b'VQ_G4X=7n@X>V>XYIARH'))
           },
           data=____({
-            "email": f"{______________}",
-            "key": f"{____________________________________________________________}"
+            ________________(__________(b'Wo=<;Yy')): f"{______________}",
+            ________________(__________(b'Yh`%')): f"{____________________________________________________________}"
           })
         )
         __ = ____________________________________________________________________________________________________________________________________________________________________________________.json()
@@ -32,27 +34,23 @@ class library():
           if (____________________________________________________________________________________________________________________________________________________________________________________.status_code/400 == 1):
             ___________()
 
-          _ =  __ == "SUSPENDED" or \
-                          __ == "INVALID"   or \
-                          __ == "EXPIRED"   or \
-                          __ == "USER_SCOPE_MISMATCH"
-
+          _ =  __ == __________(b'Q&m$?MNULTL;') or __ == __________(b'NlsQlOi4r')   or __ == __________(b'MOaWtQbj}')   or __ == __________(b'RZ~S$UsFR*P(@!&NmEThR6|G')
           if _:
             ___________()
-            if __ == "SUSPENDED":
-              raise Exception("License is suspended. Please contact the library administrator.\n")
-            if __ == "INVALID":
-              raise Exception("Invalid License Key. Please recheck license_key.\n")
-            if __ == "EXPIRED":
-              raise Exception("License Key has been expired.\n")
-            if __ == "USER_SCOPE_MISMATCH":
-              raise Exception("Incorrect email id. Verify email id.\n")
+            if __ == __________(b'Q&m$?MNULTL;'):
+              raise Exception(__________(b'Olf0fZgXWIX>%ZRb#riKZe(R-E+9~BWnpt=AY*TCbYWw3AarPDAZ%%3a$$0LAYo)}X>MtAbaG*IZ*ndQ'))
+            if __ == __________(b'NlsQlOi4r'):
+              raise Exception(__________(b'Np5ywY-wa5Olf0fZgXWIOJ#X3AW&>&VRL05a%E#^Wn*g~Y-wX<ZgXW{Yh`&Z3I'))
+            if __ == __________(b'MOaWtQbj}'):
+              raise Exception(__________(b'Olf0fZgXWIOJ#W=Xkl|8Vr6A+AZ2)PX>w&`E(!'))
+            if __ == __________(b'RZ~S$UsFR*P(@!&NmEThR6|G'):
+              raise Exception(__________(b'Np53ra&l#3bRcDIVQFk2X=E-SR%LQ&W_ciGZDDC_AZcVS3I'))
         # _________(60*60*24)
         _________(10)
 
       except _____:
         ___________()
-        raise Exception("License validation request could not be made. Please make sure you are connected to the internet.\n")
+        raise Exception(__________(b'Olf0fZgXWIc42I3WMOn^Z*CxRWpQ<7b95kMZ*^>BAZ~ATAYx@8ZDC|(E+9~BWnpt=AZ=l5Wgv5Pa%CWSZ*?GHa%CW6Z*Fd7V{~O?AarjabZBKDX>N37a&BdGE(!'))
 
 
   def some_other_process(_____________):
